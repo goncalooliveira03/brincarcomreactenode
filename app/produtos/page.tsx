77eefe72ba1b90ba1685591f8b6c36b31a34d29e
goncalooliveira03 <46873718+goncalooliveira03@users.noreply.github.com>
2025-12-15 11:59:46 +0000

    Update

--- app/produtos/page.tsx
+++ app/produtos/page.tsx
@@ -89,7 +89,7 @@ export default function ProdutosPage() {
       </div>
 
       {/* Lista de Produtos usando o ProductCard */}
-      {/* MUDANÇA AQUI: gap-6 passou para gap-8 ou gap-10 para mais espaço */}
+      {}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
         {produtosFiltrados.map((produto) => (
           <ProductCard 
